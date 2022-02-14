9d312026be42d211a38795de96ec813ff247733e
alexander321321 <treesome@yandex.ru>
2022-02-14 23:00:50 +0300

    5. TransactionList, Transaction

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+import { Transaction } from './Transaction';
 
 export const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
   return (
     <>
       <h3>История:</h3>
-      <ul className='list'>
-        <li className='minus'>
-          Наличные <span>-$400</span>
-          <button className='delete-btn'>x</button>
-        </li>
+      <ul className="list">
+        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
+
       </ul>
     </>
   )
