1aadf12c7c25704288d9cad35f10f0c691bbf8bd
alexander321321 <treesome@yandex.ru>
2022-02-14 22:06:13 +0300

    3. AddTransaction: hooks on form

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const AddTransaction = () => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+
   return (
     <>
       <h3>Добавить новую транзакцию</h3>
       <form>
         <div className="form-control">
           <label htmlFor="text">Текст</label>
-          <input type="text" id="text" placeholder="Введите текст..." />
+          <input
+            type="text"
+            id="text"
+            placeholder="Введите текст..."
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
         </div>
         <div className="form-control">
           <label htmlFor="amount">Сумма <br />
             (отрицательная- расходы, положительная- доходы)
           </label>
-          <input type="number" id="amount" placeholder="Введите сумму..." />
+          <input
+            type="number"
+            id="amount"
+            placeholder="Введите сумму..."
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)} />
         </div>
         <button className="btn">Добавить транзакцию</button>
       </form>
